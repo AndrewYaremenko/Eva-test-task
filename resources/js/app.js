677d2092925c168d95580f5bf5c6d8d0a166b369
unknown <esdesuagk@gmail.com>
2023-10-09 00:54:10 +0300

    refactoring

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,13 @@
+import $ from 'jquery';
 import './bootstrap';
 import { createApp } from 'vue';
-import App from './vue/App.vue';
 import router from './vue/routes/routes';
 import store from './vue/store/store';
 
-const app = createApp(App);
+window.jQuery = $;
+window.$ = $;
+
+const app = createApp();
 app.use(router);
 app.use(store);
 app.mount("#app");
--- resources/js/vue/components/admin/ui/AdminNavComponent.vue
+++ resources/js/vue/components/admin/ui/AdminNavComponent.vue
@@ -3,28 +3,16 @@
     <div class="container">
       <router-link class="navbar-brand" to="/admin">Dashboard</router-link>
 
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <router-link class="nav-link" to="/admin/salons">Salons</router-link>
-          <router-link class="nav-link" to="/admin/services"
-            >Services</router-link
-          >
-          <router-link class="nav-link" to="/admin/appointments"
-            >Appointments</router-link
-          >
-        </div>
+      <div class="navbar-nav">
+        <router-link class="nav-link" to="/admin/salons">Salons</router-link>
+        <router-link class="nav-link" to="/admin/services"
+          >Services</router-link
+        >
+        <router-link class="nav-link" to="/admin/appointments"
+          >Appointments</router-link
+        >
       </div>
+      
     </div>
   </nav>
 </template>
